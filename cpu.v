489c1930fa5535eeb0e038bdb5b10bb34ffafe1f
Linus Kundur-Zourntos <127998928+kundurz@users.noreply.github.com>
2025-11-09 13:17:15 -0500

    Make CPU design more modular with dedicated ALU module.

--- cpu.v
+++ cpu.v
@@ -1,4 +1,3 @@
-
 module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   // Hardware I/O
   input [9:0] SW;    // Machine code expressed on switches.
@@ -42,15 +41,16 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   reg [31:0] register_value_2;
  
   // Control to ALU
-  //reg execute_flag;
-  reg [31:0] arithmetic_result;
+  reg execute_flag;
+  wire [31:0] arithmetic_result;
  
   // ALU instance
-  //ALU alu_inst(mode, opcode, register_value_1, register_value_2, execute_flag, arithmetic_result);
+  ALU alu_inst(execute_flag, clock_pulse, LEDR[9], IR, opcode, arithmetic_result, register_value_1, register_value_2);
  
   // Display R1 on the hex display.
   display_hex hex_displayer1(R1, HEX0);
   display_hex hex_displayer2(R2, HEX1);
+
   // Next-state + dedicated stage logic: On each clock pulse, go to the next stage of the fetch-execute loop.
   always @ (negedge clock_pulse, negedge resetn) begin // account for resetn? then it just turns everything to 0.
     if (!resetn) begin
@@ -60,7 +60,7 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
       register_encoding_2 <= 0;
       register_value_1 <= 0;
       register_value_2 <= 0;
-      //execute_flag <= 0;
+      execute_flag <= 0;
  
       R1 <= 0;
       R2 <= 0;
@@ -83,17 +83,10 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
         register_value_2 <= (IR[1:0]== 2'b00) ? R1 : R2;
  
         next_state = E;
+        execute_flag <= 1;
       end
       E: begin
-// ALU Logic!
-        case (opcode)
-        ADD: begin
-          arithmetic_result = ((IR[3:2] == 2'b00) ? register_value_1 : register_value_2) + ((IR[1:0] == 2'b00) ? register_value_1 : register_value_2);
-        end
-        INC: begin
-          arithmetic_result = register_value_1 + 1; 
-        end
-        endcase
+        execute_flag <= 0;
         next_state = W;
       end
       W: begin
@@ -105,54 +98,50 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
       end
       default: next_state = F;
     endcase end
-end
+  end
  
-// State Flip-Flops
-always @ (posedge clock_pulse, negedge resetn) begin
-if (!resetn)
-present_state <= F;
-else
-present_state <= next_state;
-end
+  // State Flip-Flops
+  always @ (posedge clock_pulse, negedge resetn) begin
+    if (!resetn)
+      present_state <= F;
+    else
+      present_state <= next_state;
+  end
  
-// assign LEDR values to the current state value.
-assign LEDR[1:0] = present_state;
-assign LEDR[9:2] = opcode;
+  // assign LEDR values to the current state value.
+  assign LEDR[1:0] = present_state;
 endmodule
- 
-/*
-   Capable of performing two instructions:
-    - add regA, regB (Adds values in regA and regB, then place tha value in regA)
-    - inc reg (adds 1 to value inside of reg, then copies it back.)
- */
-//module ALU(mode, opcode, register_value_1, register_value_2, execute_flag, result);
-//  input mode;
-//  input execute_flag;
-//  input [2:0] opcode;
-//  input [31:0] register_value_1;
-//  input [31:0] register_value_2;
-//
-//  output reg [31:0] result; // first two bits are the destination register, rest are the actual result.
-//
-//  parameter [2:0] ADD = 3'b001,
-//                  INC = 3'b011;
-//
-//  // The ALU will only fire up once it is given the OK by the execute flag.
-//  always @ (posedge execute_flag) begin // experiment with posedge execute_flag.
-//    result = 32'b1;
-//    //if (execute_flag)
-//      case (opcode)
-//        ADD: begin
-//          result = register_value_1 + register_value_2;
-//        end
-//
-//        INC: begin
-//          result = register_value_1 + 1;
-//        end
-//      endcase
-//  end
-//endmodule
- 
+
+/* This is the ALU module */
+/* Capable of performing ADD and INC instructions */
+module ALU(execute_flag, clock, light, IR, opcode, arithmetic_result, register_value_1, register_value_2);
+  parameter [2:0] ADD = 3'b001,
+                  INC = 3'b011;
+  input [7:0] IR;
+
+  input execute_flag;
+  input clock;
+  input [2:0] opcode;
+  input [31:0] register_value_1;
+  input [31:0] register_value_2;
+  
+  output reg light;
+  output reg [31:0] arithmetic_result;
+
+  always @ (negedge clock) begin
+    if (execute_flag) begin
+      light <= 1;
+      case (opcode) 
+      ADD: arithmetic_result = ((IR[3:2] == 2'b00) ? register_value_1 : register_value_2) + ((IR[1:0] == 2'b00) ? register_value_1 : register_value_2);
+      INC: arithmetic_result = register_value_1 + 1; 
+      endcase
+    end
+    else 
+      light <= 0;
+  end
+endmodule
+
+/* Hex display module for displaying first few bytes of the registers on the hex displays*/
 module display_hex(input [3:0] dig, output [6:0] HEX);
     reg [6:0] temp;
     assign HEX = temp;   // connect internal signal to output
