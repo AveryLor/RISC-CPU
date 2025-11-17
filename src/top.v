1c7032680f8c6587b40594e71e7be0e6f86c05dd
Avery Lor <104920232+AveryLor@users.noreply.github.com>
2025-11-17 12:22:58 -0500

    Add registers to vga_display module connection

--- src/top.v
+++ src/top.v
@@ -15,8 +15,22 @@ module top(CLOCK_50, KEY, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_
 	output [9:0] LEDR; // LEDR used to display register values (first 4 bits of R1 and R2).
 	output [6:0] HEX0;
 	output [6:0] HEX1;
+	wire [31:0] RD1, RD2; 
 	
+	wire [31:0] registers [7:0];
+	
+	assign registers[0] = 0;
+   assign registers[1] = 32'hAAAA0001;
+   assign registers[2] = 32'hAAAA0002;
+   assign registers[3] = 32'hAAAA0003;
+   assign registers[4] = 32'hAAAA0004;
+   assign registers[5] = 32'hAAAA0005;
+   assign registers[6] = 32'hAAAA0006;
+   assign registers[7] = 32'hAAAA0007;
+	
+	register_file rf (CLOCK_50, 1'b1, 1'b1, 3'b000, 3'b000, 3'b000, 32'b0, RD1, RD2); 
 	control_unit cu (SW, LEDR, KEY, HEX0, HEX1); 
-	vga_display vd (CLOCK_50, KEY, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK); 
+	vga_display vd (CLOCK_50, KEY, VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK, registers); 
+	
 
 endmodule
