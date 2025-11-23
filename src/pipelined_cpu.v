3a50306dc3fa7f454059e02fdbaed7b09b951355
Linus Kundur-Zourntos <127998928+kundurz@users.noreply.github.com>
2025-11-22 23:15:08 -0500

    Updated instruction fetch with buffer stage to fix bug

--- src/pipelined_cpu.v
+++ src/pipelined_cpu.v
@@ -12,15 +12,19 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   // Clock pulse and reset
   wire clock_pulse = ~KEY[0];
   wire resetn = KEY[1];
-  wire stall = ~KEY[2]; // This will be changed later. It is kept just for testing the module.
+  wire stall; // Controlled by HDU.
+
+  //assign stall = 0;
 
   // Registers
   wire [31:0] IR; // Special purpose
-  /* Should add 16-bit pc register when program counter is implemented. */
   
   // Pipeline registers
-  wire [7:0] if_id_reg;
- 
+  // IF/ID Registers
+  wire [7:0] if_id_reg; // Doubles as ID_instruct
+  
+  // ID/EX Registers
+  wire [7:0] ex_instruct; 
   wire id_ex_regwrite;
   wire id_ex_reg_mode;
   wire [2:0] id_ex_reg_opcode;
@@ -28,10 +32,14 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   wire [31:0] id_ex_reg_val2;
   wire [1:0] id_ex_reg_wb_enc; 
   
+  // EX/MEM Registers
+  wire [7:0] mem_instruct;
   wire ex_mem_regwrite;
   wire [31:0] ex_mem_reg_arithmetic_result;
   wire [1:0] ex_mem_reg_wb_enc;
-
+  
+  // MEM/WB Registers
+  wire [7:0] wb_instruct;
   wire mem_wb_regwrite;
   wire [1:0] mem_wb_reg_wb_enc;
   wire [31:0] mem_wb_reg_arithmetic_result;
@@ -80,6 +88,8 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   wire [31:0] alu_reg_val2;
   wire [31:0] alu_result; 
 
+  wire bs;
+
   // ALU instantiation
   ALU alu_inst(
     .opcode(alu_opcode),
@@ -87,17 +97,31 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
     .register_value_1(alu_reg_val1),
     .register_value_2(alu_reg_val2)
   );
+
+
+  HDU hdu_inst(
+    .ID_instruct(if_id_reg),
+    .EX_instruct(ex_instruct),
+    .MEM_instruct(mem_instruct),
+    .WB_instruct(wb_instruct),
+
+    .stall(stall),
+    .out(bs)
+  );
   
+
   instr_fetch instr_fetch_inst(
       .clk(clock_pulse),
-      .stall(stall)
-	  .pc(), // add 16-bit program counter here
+      .stall(stall),
+      .resetn(resetn),
+
       .if_id_reg(if_id_reg)
   );
 
   instr_decode instr_decode_inst(
       .clk(clock_pulse),
       .stall(stall),
+      .resetn(resetn),
 
       .rf_enc_0(r_enc_0),
       .rf_enc_1(r_enc_1),
@@ -112,11 +136,14 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
       .id_ex_reg_mode(id_ex_reg_mode),
       .id_ex_reg_opcode(id_ex_reg_opcode),
       .id_ex_reg_wb_enc(id_ex_reg_wb_enc),
-      .id_ex_regwrite(id_ex_regwrite)
+      .id_ex_regwrite(id_ex_regwrite),
+
+      .ex_instruct(ex_instruct)
   );
 
   instr_execute instr_execute_inst(
       .clk(clock_pulse),
+      .resetn(resetn),
 
       .alu_opcode(alu_opcode),
       .alu_reg_val1(alu_reg_val1),
@@ -131,11 +158,15 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
 
       .ex_mem_reg_wb_enc(ex_mem_reg_wb_enc),
       .ex_mem_regwrite(ex_mem_regwrite),
-      .ex_mem_reg_arithmetic_result(ex_mem_reg_arithmetic_result)
+      .ex_mem_reg_arithmetic_result(ex_mem_reg_arithmetic_result),
+
+      .ex_instruct(ex_instruct),
+      .mem_instruct(mem_instruct)
   ); 
   
   instr_mem instr_mem_inst(
       .clk(clock_pulse),
+      .resetn(resetn),
 
       .ex_mem_regwrite(ex_mem_regwrite),
       .ex_mem_reg_wb_enc(ex_mem_reg_wb_enc),
@@ -143,7 +174,10 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
 
       .mem_wb_reg_wb_enc(mem_wb_reg_wb_enc),
       .mem_wb_reg_arithmetic_result(mem_wb_reg_arithmetic_result),
-      .mem_wb_regwrite(mem_wb_regwrite)
+      .mem_wb_regwrite(mem_wb_regwrite),
+
+      .mem_instruct(mem_instruct),
+      .wb_instruct(wb_instruct)
 
   );
 
@@ -160,17 +194,14 @@ module control_unit(SW, LEDR, KEY, HEX0, HEX1);
   );
   
   // Testing decode: 
-  assign LEDR[2:0] = ex_mem_reg_arithmetic_result;
-  assign LEDR[8:7] = mem_wb_reg_wb_enc;
-  assign LEDR[9] = mem_wb_regwrite;
-  //assign LEDR[9:7] = if_id_reg[6:4];
-  //assign LEDR[6:4] = id_ex_reg_opcode; 
-  //assign LEDR[3:2] = ex_mem_regwrite;
-  //assign LEDR[1:0] = mem_wb_regwrite;
+  assign LEDR[9:8] = if_id_reg[5:4];
+  assign LEDR[7:6] = ex_instruct[5:4];
+  assign LEDR[5:4] = mem_instruct[5:4];
+  assign LEDR[3:2] = wb_instruct[5:4];
 endmodule
 
 // Register file module 
-module reg_file(clk, resetn, we, r_enc_0, r_enc_1, r_write_enc, reg_out_0, reg_out_1, wdata, R0_val, R1_val, register_output); 
+module reg_file(clk, resetn, we, r_enc_0, r_enc_1, r_write_enc, reg_out_0, reg_out_1, wdata, R0_val, R1_val); 
   input we; // Indicates if we are doing a write (write enable)
   input clk;
   input resetn;
@@ -183,8 +214,6 @@ module reg_file(clk, resetn, we, r_enc_0, r_enc_1, r_write_enc, reg_out_0, reg_o
   output [31:0] R0_val;
   output [31:0] R1_val;
   
-  output [31:0] register_output[7:0];
-
   output reg [31:0] reg_out_0;
   output reg [31:0] reg_out_1;
   
@@ -200,10 +229,10 @@ module reg_file(clk, resetn, we, r_enc_0, r_enc_1, r_write_enc, reg_out_0, reg_o
   end
   
   // Writing to registers.
-  always @ (posedge clk or negedge resetn) begin
+  always @ (negedge clk or negedge resetn) begin
     if (!resetn) begin
-      R0 <= 32'd3;
-      R1 <= 32'd3;
+      R0 <= 32'd0;
+      R1 <= 32'd0;
     end
     else if (we) begin
       if (r_write_enc == 2'b00) R0 <= wdata;
@@ -211,17 +240,9 @@ module reg_file(clk, resetn, we, r_enc_0, r_enc_1, r_write_enc, reg_out_0, reg_o
     end
   end
 
-  assign register_output[0] = R0;
-  assign register_output[1] = R1;
-  assign register_output[2] = R2; 
-  assign register_output[3] = R3;
-  assign register_output[4] = R4;
-  assign register_output[5] = R5;
-  assign register_output[6] = R6;
-  assign register_output[7] = R7;
-
+  assign R0_val = R0; 
+  assign R1_val = R1;
 endmodule
-
 
 // ALU Module
 module ALU(opcode, arithmetic_result, register_value_1, register_value_2);
@@ -242,49 +263,56 @@ module ALU(opcode, arithmetic_result, register_value_1, register_value_2);
   end
 endmodule
 
-
-module instr_fetch(clk, stall, pc, if_id_reg);
+module instr_fetch(clk, resetn, stall, if_id_reg);
   input clk;
+  input resetn;
   input stall;
-  input [15:0] pc;				// program counter (new)
-  output [31:0] if_id_reg;		// changed to 32 bits. instead of a reg, this is now a wire to a BRAM DataOut reg.
-  // removed input [7:0] switches_state
-  
-  /* START OF NEW ADDITIONS */
-  // This module will be defined by quartus. 
-  // In Quartus, name the BRAM instr_rom, set width = 32, depth = 65536, use a single clock, and initialize it with a .mif.
+  reg [15:0] pc;				// program counter (new)
+  output reg [7:0] if_id_reg;		// changed to 32 bits. instead of a reg, this is now a wire to a BRAM DataOut reg.
+
+  wire [7:0] instr_rom_out;
   
   instr_rom instr_rom(
 	.address(pc),
 	.clock(clk),
-	.data()
+	.data(),
 	.wren(0),
-	.q(if_id_reg);
-
-	input	[15:0]  address;
-	input	clock;
-	input	[7:0]  data;
-	input	wren;
-	output	[7:0]  q;
-  );
+	.q(instr_rom_out)
+	
+	);
   
-  /* END OF NEW ADDITIONS */
-
-  // If you want to stall here, just keep the program counter static instead.
-  /*
-  always @ (posedge clk) begin 
-    if (stall == 0) if_id_reg <= switches_state;
-    else if_id_reg <= if_id_reg;
+  reg load_enable;
+  always @ (posedge clk or negedge resetn) begin
+    if (!resetn) begin 
+      pc <= 0;
+      if_id_reg <= 0;
+      load_enable <= 1;
+    end
+    else if (stall) begin 
+      pc <= pc;
+      if (load_enable) begin
+        if_id_reg <= instr_rom_out;
+        load_enable <= 0;
+      end
+      else if_id_reg <= if_id_reg;
+    end
+    else begin
+      pc <= pc + 1;
+      if_id_reg <= instr_rom_out;
+      load_enable <= 1;
+    end
   end
-  */
 endmodule
 
-module instr_decode(clk, stall, rf_enc_0, rf_enc_1, rf_out_val1, rf_out_val2, if_id_reg, id_ex_reg_val1, id_ex_reg_val2, id_ex_reg_mode, id_ex_reg_opcode, id_ex_reg_wb_enc, id_ex_regwrite);
+module instr_decode(clk, resetn, stall, rf_enc_0, rf_enc_1, rf_out_val1, rf_out_val2, if_id_reg, id_ex_reg_val1, id_ex_reg_val2, id_ex_reg_mode, id_ex_reg_opcode, id_ex_reg_wb_enc, id_ex_regwrite, ex_instruct);
+  parameter [2:0] NOP = 3'b000;
+
   input clk;
   input stall;
+  input resetn;
+
   input [7:0] if_id_reg;
 
-  
   output reg id_ex_reg_mode;
   output reg [2:0] id_ex_reg_opcode;
   output reg [1:0] id_ex_reg_wb_enc;
@@ -292,6 +320,8 @@ module instr_decode(clk, stall, rf_enc_0, rf_enc_1, rf_out_val1, rf_out_val2, if
 
   output reg [31:0] id_ex_reg_val1;
   output reg [31:0] id_ex_reg_val2;
+
+  output reg [7:0] ex_instruct;
 
   // Register file control
   output [1:0] rf_enc_0;
@@ -305,8 +335,17 @@ module instr_decode(clk, stall, rf_enc_0, rf_enc_1, rf_out_val1, rf_out_val2, if
   assign rf_enc_1 = if_id_reg[1:0];
   
   // values will be given by the register file once it is instantiated.
-  always @ (posedge clk) begin
-    if (!stall) begin
+  always @ (posedge clk or negedge resetn) begin
+    if (!resetn) begin
+      id_ex_reg_mode <= 1'b0;
+      id_ex_reg_opcode <= NOP;
+      id_ex_reg_wb_enc <= 2'b00;
+      id_ex_reg_val1 <= 32'd0;
+      id_ex_reg_val2 <= 32'd0;
+      id_ex_regwrite <= 1'b0;
+      ex_instruct <= {1'b0, NOP, 2'b00, 2'b00};
+    end
+    else if (!stall) begin
       id_ex_reg_mode <= if_id_reg[7];
       id_ex_reg_opcode <= if_id_reg[6:4];
       id_ex_reg_wb_enc <= if_id_reg[3:2];
@@ -314,14 +353,25 @@ module instr_decode(clk, stall, rf_enc_0, rf_enc_1, rf_out_val1, rf_out_val2, if
       id_ex_reg_val1 <= rf_out_val1;
       id_ex_reg_val2 <= rf_out_val2;
 
+      ex_instruct <= if_id_reg;
       if (if_id_reg[6:4] != 3'b000) id_ex_regwrite <= 1;
       else id_ex_regwrite <= 0;
+    end
+    else begin
+      id_ex_reg_mode <= 1'b0;
+      id_ex_reg_opcode <= NOP;
+      id_ex_reg_wb_enc <= 2'b00;
+      id_ex_reg_val1 <= 32'd0;
+      id_ex_reg_val2 <= 32'd0;
+      id_ex_regwrite <= 1'b0;
+      ex_instruct <= {1'b0, NOP, 2'b00, 2'b00};
     end
   end
 endmodule
 
-module instr_execute(clk, alu_opcode, alu_reg_val1, alu_reg_val2, alu_result, id_ex_reg_opcode, id_ex_reg_val1, id_ex_reg_val2, id_ex_regwrite, id_ex_reg_wb_enc, ex_mem_reg_wb_enc, ex_mem_regwrite, ex_mem_reg_arithmetic_result);
+module instr_execute(clk, resetn, alu_opcode, alu_reg_val1, alu_reg_val2, alu_result, id_ex_reg_opcode, id_ex_reg_val1, id_ex_reg_val2, id_ex_regwrite, id_ex_reg_wb_enc, ex_mem_reg_wb_enc, ex_mem_regwrite, ex_mem_reg_arithmetic_result, ex_instruct, mem_instruct);
   input clk;
+  input resetn;
   
   // From ID/EX pipeline
   input        id_ex_regwrite;
@@ -329,11 +379,15 @@ module instr_execute(clk, alu_opcode, alu_reg_val1, alu_reg_val2, alu_result, id
   input [2:0]  id_ex_reg_opcode;
   input [31:0] id_ex_reg_val1;
   input [31:0] id_ex_reg_val2;
+  
+  input [7:0] ex_instruct;
 
   // EX/MEM pipeline registers
   output reg [1:0] ex_mem_reg_wb_enc;
   output reg ex_mem_regwrite;
   output reg [31:0] ex_mem_reg_arithmetic_result;
+
+  output reg [7:0] mem_instruct;
   
   // To ALU (combinational - wires)  
   output [2:0] alu_opcode;
@@ -348,30 +402,53 @@ module instr_execute(clk, alu_opcode, alu_reg_val1, alu_reg_val2, alu_result, id
   assign alu_reg_val1 = id_ex_reg_val1;
   assign alu_reg_val2 = id_ex_reg_val2;
 
-  always @ (posedge clk) begin
-    ex_mem_reg_wb_enc <= id_ex_reg_wb_enc;
-    ex_mem_regwrite <= id_ex_regwrite;
-    ex_mem_reg_arithmetic_result <= alu_result;
+  always @ (posedge clk or negedge resetn) begin
+    if (!resetn) begin
+      ex_mem_reg_wb_enc <= 2'b00;
+      ex_mem_regwrite <= 1'b0;
+      ex_mem_reg_arithmetic_result <= 32'd0;
+      mem_instruct <= 8'b0;
+    end
+    else begin
+      ex_mem_reg_wb_enc <= id_ex_reg_wb_enc;
+      ex_mem_regwrite <= id_ex_regwrite;
+      ex_mem_reg_arithmetic_result <= alu_result;
+      mem_instruct <= ex_instruct;
+    end
   end
 endmodule
 
-module instr_mem(clk, ex_mem_regwrite, ex_mem_reg_wb_enc, ex_mem_reg_arithmetic_result, mem_wb_reg_wb_enc, mem_wb_reg_arithmetic_result, mem_wb_regwrite);
+module instr_mem(clk, resetn, ex_mem_regwrite, ex_mem_reg_wb_enc, ex_mem_reg_arithmetic_result, mem_wb_reg_wb_enc, mem_wb_reg_arithmetic_result, mem_wb_regwrite, mem_instruct, wb_instruct);
+  parameter [2:0] NOP = 3'b000;
   input clk;
+  input resetn;
+
   input [1:0] ex_mem_reg_wb_enc;
   input [31:0] ex_mem_reg_arithmetic_result;
   input ex_mem_regwrite;
+  input [7:0] mem_instruct;
   
   output reg [1:0] mem_wb_reg_wb_enc;
   output reg [31:0] mem_wb_reg_arithmetic_result;
   output reg mem_wb_regwrite;
+  output reg [7:0] wb_instruct;
   
-  always @ (posedge clk) begin
-    mem_wb_reg_wb_enc <= ex_mem_reg_wb_enc;
-    mem_wb_reg_arithmetic_result <= ex_mem_reg_arithmetic_result;
-    mem_wb_regwrite <= ex_mem_regwrite;
+  always @ (posedge clk or negedge resetn) begin
+    if (!resetn) begin
+      mem_wb_reg_wb_enc <= 0;
+      mem_wb_reg_arithmetic_result <= 0;
+      mem_wb_regwrite <= 0;
+      wb_instruct <= {1'b0, NOP, 2'b00, 2'b00};
+    end
+    else begin
+      mem_wb_reg_wb_enc <= ex_mem_reg_wb_enc;
+      mem_wb_reg_arithmetic_result <= ex_mem_reg_arithmetic_result;
+      mem_wb_regwrite <= ex_mem_regwrite;
+
+      wb_instruct <= mem_instruct;
+    end
   end
 endmodule
-
 
 module instr_wb(clk, mem_wb_regwrite, mem_wb_reg_wb_enc, mem_wb_reg_arithmetic_result, rf_we, rf_w_enc, rf_wdata); 
   input clk;
@@ -389,6 +466,58 @@ module instr_wb(clk, mem_wb_regwrite, mem_wb_reg_wb_enc, mem_wb_reg_arithmetic_r
     rf_w_enc <= mem_wb_reg_wb_enc; 
     rf_wdata <= mem_wb_reg_arithmetic_result;
   end
+endmodule
+
+module HDU(ID_instruct, EX_instruct, MEM_instruct, WB_instruct, stall, out);
+  // I am going to assume we shove the whole instruction through the pipeline.
+  parameter [2:0] NOP = 3'b000,
+                  ADD = 3'b001,
+                  INC = 3'b011;
+
+  input [7:0] ID_instruct;
+  input [7:0] EX_instruct;
+  input [7:0] MEM_instruct;
+  input [7:0] WB_instruct; 
+
+  output reg stall; // This is the stall signal!
+  output out;
+
+  wire [2:0] ID_opcode;
+  wire [1:0] ID_reg1;
+  wire [1:0] ID_reg2;
+  
+  wire [2:0] EX_opcode;
+  wire [1:0] EX_reg1; 
+
+  wire [2:0] MEM_opcode;
+  wire [1:0] MEM_reg1;
+
+  wire [2:0] WB_opcode;
+  wire [1:0] WB_reg1;
+
+  // have some assigns with the inputs to the wires
+  assign ID_opcode = ID_instruct[6:4];
+  assign EX_opcode = EX_instruct[6:4];
+  assign MEM_opcode = MEM_instruct[6:4];
+  assign WB_opcode = WB_instruct[6:4];
+
+  assign ID_reg1 = ID_instruct[3:2]; 
+  assign ID_reg2 = ID_instruct[1:0];
+
+  assign EX_reg1 = EX_instruct[3:2];
+  assign MEM_reg1 = MEM_instruct[3:2];
+  assign WB_reg1 = WB_instruct[3:2];
+
+
+  always @ (*) begin
+    if (ID_opcode != NOP) begin
+      if ((EX_opcode != NOP && EX_reg1 == ID_reg1) || (MEM_opcode != NOP && MEM_reg1 == ID_reg1) || (WB_opcode != NOP && WB_reg1 == ID_reg1)) stall = 1;
+      else if (ID_opcode != INC && ((EX_opcode != NOP && ID_reg2 == EX_reg1) || (MEM_opcode != NOP && ID_reg2 == MEM_reg1) || (WB_opcode != NOP && ID_reg2 == WB_reg1))) stall = 1;
+      else stall = 0;
+    end
+    else stall = 0;
+  end
+  assign out = stall;
 endmodule
 
 /* Hex display module for displaying first few bytes of the registers on the hex displays*/
