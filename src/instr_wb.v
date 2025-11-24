42072c34e4d834905ce1a965308bafb721a46e31
ilia-jpg <ilia.javan.eng@gmail.com>
2025-11-24 17:30:22 -0500

    Update instr_wb.v

--- src/instr_wb.v
+++ src/instr_wb.v
@@ -1,22 +1,22 @@
 module instr_wb(
-	input clock,
-	input reset,
+	input clk,
+	input resetn,
 
-	input [31:0] mem_wb_full_instruction,		// For VGA
-	input [1:0] register_writeback_enable,
-	input [2:0] writeback_register_encoding,	// From decode stage
+	input [31:0] wb_instruct,		// For VGA
+	input [1:0] mem_wb_regwrite,
+	input [2:0] mem_wb_reg_wb_enc,	// From decode stage
 	
-	input [31:0] memory_writeback_data,			// From memory stage
-	input [31:0] arithmetic_writeback_data,		// From execute stage
-	input [31:0] move_writeback_data, 			// From decode stage (operand_value_2)
-	input [2:0] writeback_data_select_hotcode,
+	input [31:0] mem_wb_reg_memory_wb_data,			// From memory stage
+	input [31:0] mem_wb_reg_arithmetic_result,		// From execute stage
+	input [31:0] mem_wb_reg_operand_val2, 			// From decode stage (operand_value_2)
+	input [2:0] mem_wb_reg_data_select_hotcode,
 
 	/* Signals to register file for writeback */
 	output reg [1:0] reg_file_write_enable,
 	output reg [2:0] reg_file_register_encoding,
 	output reg [31:0] reg_file_writeback_data,
 	
-	output reg[31:0] wb_full_instruction		// For VGA
+	output reg[31:0] completed instruction		// For VGA
 );
 
 parameter [2:0] is_arithmetic = 3'b100,
@@ -24,20 +24,18 @@ parameter [2:0] is_arithmetic = 3'b100,
 				is_move       = 3'b001;
 
 
-always@(posedge clock, posedge reset) begin
-	if (reset) reg_file_write_enable <= 0; else begin
-		case (writeback_data_select_hotcode)
-			is_arithmetic: 	reg_file_writeback_data <= arithmetic_writeback_data;
-			is_memory:		reg_file_writeback_data <= memory_writeback_data;
-			is_move:		reg_file_writeback_data <= move_writeback_data;
+always@(posedge clk, negedge resetn) begin
+	if (resetn) reg_file_write_enable <= 0; else begin
+		case (mem_wb_reg_data_select_hotcode)
+			is_arithmetic: 	reg_file_writeback_data <= mem_wb_reg_arithmetic_result;
+			is_memory:		reg_file_writeback_data <= mem_wb_reg_memory_wb_data;
+			is_move:		reg_file_writeback_data <= mem_wb_reg_operand_val2;
 			default:		reg_file_writeback_data <= 0; // unused
 		endcase
-		reg_file_write_enable <= register_writeback_enable;
-		reg_file_register_encoding <= writeback_register_encoding;
+		reg_file_write_enable <= mem_wb_regwrite;
+		reg_file_register_encoding <= mem_wb_reg_wb_enc;
 		
-		wb_full_instruction <= mem_wb_full_instruction; // for VGA
+		completed_instruction <= wb_instruct; // for VGA
 	end 
 end
-
-
 endmodule
