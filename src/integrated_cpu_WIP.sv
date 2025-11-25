62215bbba202cdc7669c30e23a72cca8e51b9233
Linus Kundur-Zourntos <127998928+kundurz@users.noreply.github.com>
2025-11-25 00:19:11 -0500

    Refactor control_unit module and clean up code
    Refactor control unit module for improved readability and organization. Update display assignments and clean up commented-out code.

--- src/integrated_cpu_WIP.sv
+++ src/integrated_cpu_WIP.sv
@@ -1,13 +1,15 @@
-module control_unit(SW, LEDR, KEY, HEX5, HEX4, HEX0, HEX1, HEX3, HEX2);
-	  // Hardware I/O
-  input [9:0] SW;
-  input [2:0] KEY;
-  output [9:0] LEDR;
-  output [6:0] HEX0;
-  output [6:0] HEX1;
-  output [6:0] HEX3;
-  output [6:0] HEX2, HEX5, HEX4;
-
+module control_unit(
+	// Hardware I/O
+	input [9:0] SW,
+	input [2:0] KEY,
+	output [9:0] LEDR,
+	output [6:0] HEX5,
+	output [6:0] HEX4,
+	output [6:0] HEX3,
+	output [6:0] HEX2, 
+	output [6:0] HEX1,
+	output [6:0] HEX0 
+);
   // Necessary values
   // wire [7:0] instruction_state = SW[7:0]; <- not used, can remove
 
@@ -15,12 +17,17 @@ module control_unit(SW, LEDR, KEY, HEX5, HEX4, HEX0, HEX1, HEX3, HEX2);
   wire clock_pulse = ~KEY[1];
   wire resetn = KEY[0];
   wire stall; // Controlled by HDU.
+  assign LEDR[0] = stall;
 
   //assign stall = 0;
 
   // Registers
   wire [31:0] IR; // Special purpose
-
+// Testing decode: 
+  assign LEDR[9:8] = if_id_reg[28:26];
+  assign LEDR[7:6] = ex_instruct[28:26];
+  assign LEDR[5:4] = mem_instruct[28:26];
+  assign LEDR[3:2] = wb_instruct[28:26];
   
   // Register file conntrol
   wire [1:0] we; //
@@ -63,10 +70,10 @@ module control_unit(SW, LEDR, KEY, HEX5, HEX4, HEX0, HEX1, HEX3, HEX2);
 
   
   display_hex display_hex_inst5(pc[3:0], HEX5);
-  display_hex display_hex_inst4({1'b0, id_ex_reg_wb_data_select_hotcode}, HEX4); 
-  display_hex display_hex_inst3({1'b0, ex_mem_reg_wb_data_select_hotcode}, HEX3);
-  display_hex display_hex_inst2({1'b0, mem_wb_reg_wb_data_select_hotcode}, HEX2);  
-  display_hex display_hex_inst1(4'he, HEX1);
+  display_hex display_hex_inst4(if_id_reg[3:0], HEX4); 
+  display_hex display_hex_inst3(ex_instruct[3:0], HEX3);
+  display_hex display_hex_inst2(mem_instruct[3:0], HEX2);  
+  display_hex display_hex_inst1(wb_instruct[3:0], HEX1);
   display_hex display_hex_inst0(R0_val, HEX0);  
   
   
@@ -272,11 +279,7 @@ module control_unit(SW, LEDR, KEY, HEX5, HEX4, HEX0, HEX1, HEX3, HEX2);
   
   wire [31:0] completed_instruction;
   
-  // Testing decode: 
-  assign LEDR[9:8] = if_id_reg[28:26];
-  assign LEDR[7:6] = ex_instruct[28:26];
-  assign LEDR[5:4] = mem_instruct[28:26];
-  assign LEDR[3:2] = wb_instruct[28:26];
+
   
   
 endmodule
@@ -410,11 +413,13 @@ module instr_fetch(clk, resetn, stall, catheter, if_id_reg, pc_out);
   input resetn;
   input stall;
   output [14:0] pc_out;
-  reg [14:0] pc;				// program counter (new)
+  reg [14:0] pc;	  // program counter (new)
   output reg [31:0] if_id_reg;		// changed to 32 bits. instead of a reg, this is now a wire to a BRAM DataOut reg.
   output [31:0] catheter; assign catheter = instr_rom_out;
   wire [31:0] instr_rom_out;
   assign pc_out = pc;
+  
+  reg [31:0] buffer;
   
   instruction_rom instruction_rom(
 	.address(pc),
@@ -426,14 +431,39 @@ module instr_fetch(clk, resetn, stall, catheter, if_id_reg, pc_out);
 	);
   
   reg load_enable;
-  
-  reg [4:0] warmup_cnt;
-  wire warmup_done = (warmup_cnt == 2'd2); // change this to <=
-  wire warmup_really_done = (warmup_cnt >= 2);
   always @ (posedge clk or negedge resetn) begin
-    if (!resetn) begin 
-      pc <= 0;
-      if_id_reg <= 0;
+	if (!resetn) begin
+		pc <= 0;
+		if_id_reg <= 0;
+		buffer <= 0;
+		load_enable <= 1;
+	end
+	else if (stall) begin
+		pc <= pc;
+		if (load_enable) begin
+			buffer <= instr_rom_out;
+			load_enable <= 0;
+		end
+		else begin
+			if_id_reg <= if_id_reg;
+			buffer <= buffer;
+		end
+		end
+	else begin
+		buffer <= instr_rom_out;
+		pc <= pc + 1;
+		if_id_reg <= buffer;
+		load_enable <= 1;
+	end
+  end
+  
+  //reg [4:0] warmup_cnt;
+  //wire warmup_done = (warmup_cnt == 2'd2); // change this to <=
+  //wire warmup_really_done = (warmup_cnt >= 2);
+  //always @ (posedge clk or negedge resetn) begin
+  //  if (!resetn) begin 
+  //    pc <= 0;
+  /*   if_id_reg <= 0;
       load_enable <= 1;
 		warmup_cnt <= 0;
     end
@@ -458,10 +488,11 @@ module instr_fetch(clk, resetn, stall, catheter, if_id_reg, pc_out);
 		if(!warmup_really_done) warmup_cnt <= warmup_cnt + 2'd1;
     end
 	 
-  end
+  end*/
 endmodule
 
-module instr_decode(
+module instr_decode
+(
 	input wire clk,
 	input wire resetn,
 	input wire stall,
@@ -920,371 +951,4 @@ module display_hex(input [3:0] dig, output [6:0] HEX);
         else
             temp = 7'b1111111;  // display off (invalid input)
     end
-endmodule
-
-module memory_io_unit (
-	/* Inputs are from the execute/memory pipeline register */
-	input [31:0] data_to_store,	// only used when storing: regsiter => memory address
-					// bytes may be unused if storing halfword & byte
-	input [4:0] memory_access_code, // from the op-code: specifies load/store & data size
-	input [31:0] memory_address,	// up to 2^18
-	input clock,			// clock is only used for storing
-	input [1:0] prev_r,			// used for loading
-	/* Output is sent DIRECTLY to writeback module, as a wire */
-	output [31:0] writeback_register_data,	// only used when loading: memory address => register
-	output [1:0] r
-);
-	
-	wire store; // 1: storing in memory, 0: loading from memory
-	assign store = memory_access_code[4];
-	wire [3:0] byte_enable; // determines which 4 bytes will be written or stored
-	assign byte_enable = memory_access_code[3:0];
-
-	// Each B-RAM uses a 16 bit address and loads/stores bytes
-	wire [15:0] B_address [3:0];	// 4 addresses for loading/storing up to 4 bytes
-	wire [7:0] B_data_to_write [3:0];
-	wire [7:0] B_data_to_read [3:0];
-	wire [3:0] B_write_enable;
-
-	// bram3 => addresses 4k + 0,
-	// bram2 => addresses 4k + 1,
-	// bram1 => addresses 4k + 2,
-	// bram0 => addresses 4k + 3, k is an integer
-	
-	// Given a memory address, we need to find the right addresses in the BRAM
-	// remainder from memory address divided by 4
-	assign r = memory_address[1:0];
-	assign B_address[3] = (r < 2'd1) ? (memory_address[17:2]) : (memory_address[17:2] + 1);
-	assign B_address[2] = (r < 2'd2) ? (memory_address[17:2]) : (memory_address[17:2] + 1); 
-	assign B_address[1] = (r < 2'd3) ? (memory_address[17:2]) : (memory_address[17:2] + 1);
-	assign B_address[0] = (memory_address[17:2]); // r < 4 always
-
-
-	// The tricky part: which BRAM corresponds to each byte of the data to write?
-	// The assignments rotate based on the remainder
-	assign B_data_to_write[3] =
-	(r == 2'd0) ? data_to_store[31:24] :
-	(r == 2'd1) ? data_to_store[ 7: 0] :
-	(r == 2'd2) ? data_to_store[15: 8] :
-	/*r == 2'd3*/ data_to_store[23:16];
-
-	assign B_data_to_write[2] =
-	(r == 2'd0) ? data_to_store[23:16] :
-	(r == 2'd1) ? data_to_store[31:24] :
-	(r == 2'd2) ? data_to_store[ 7: 0] :
-	/*r == 2'd3*/ data_to_store[15: 8];
-
-	assign B_data_to_write[1] =
-	(r == 2'd0) ? data_to_store[15: 8] :
-	(r == 2'd1) ? data_to_store[23:16] :
-	(r == 2'd2) ? data_to_store[31:24] :
-	/*r == 2'd3*/ data_to_store[ 7: 0];
-
-	assign B_data_to_write[0] =
-	(r == 2'd0) ? data_to_store[ 7: 0] :
-	(r == 2'd1) ? data_to_store[15: 8] :
-	(r == 2'd2) ? data_to_store[23:16] :
-	/*r == 2'd3*/ data_to_store[31:24];
-
-	// Similarly, the write enable will also rotate, and is only enabled if we are storing
-	assign B_write_enable[3] = store & (
-	(r == 2'd0) ? byte_enable[3] :
-	(r == 2'd1) ? byte_enable[0] :
-	(r == 2'd2) ? byte_enable[1] :
-	/*r == 2'd3*/ byte_enable[2]
-	);
-
-	assign B_write_enable[2] = store & (
-	(r == 2'd0) ? byte_enable[2] :
-	(r == 2'd1) ? byte_enable[3] :
-	(r == 2'd2) ? byte_enable[0] :
-	/*r == 2'd3*/ byte_enable[1]
-	);
-
-	assign B_write_enable[1] = store & (
-	(r == 2'd0) ? byte_enable[1] :
-	(r == 2'd1) ? byte_enable[2] :
-	(r == 2'd2) ? byte_enable[3] :
-	/*r == 2'd3*/ byte_enable[0]
-	);
-
-	assign B_write_enable[0] = store & (
-	(r == 2'd0) ? byte_enable[0] :
-	(r == 2'd1) ? byte_enable[1] :
-	(r == 2'd2) ? byte_enable[2] :
-	/*r == 2'd3*/ byte_enable[3]
-	);
-
-	// The order of consecutive bytes we want to read from the BRAM also rotates
-	assign writeback_register_data[31:24] =
-    	(prev_r == 2'd0) ? B_data_to_read[3] :
-	(prev_r == 2'd1) ? B_data_to_read[0] :
-	(prev_r == 2'd2) ? B_data_to_read[1] :
-	/*prev_r == 2'd3*/ B_data_to_read[2];
-	
-	assign writeback_register_data[23:16] =
-	(prev_r == 2'd0) ? B_data_to_read[2] :
-	(prev_r == 2'd1) ? B_data_to_read[3] :
-	(prev_r == 2'd2) ? B_data_to_read[0] :
-	/*prev_r == 2'd3*/ B_data_to_read[1];
-	
-	assign writeback_register_data[15:8] =
-	(prev_r == 2'd0) ? B_data_to_read[1] :
-	(prev_r == 2'd1) ? B_data_to_read[2] :
-	(prev_r == 2'd2) ? B_data_to_read[3] :
-	/*prev_r == 2'd3*/ B_data_to_read[0];
-	
-	assign writeback_register_data[7:0] =
-    	(prev_r == 2'd0) ? B_data_to_read[0] :
-    	(prev_r == 2'd1) ? B_data_to_read[1] :
-    	(prev_r == 2'd2) ? B_data_to_read[2] :
-	/*prev_r == 2'd3*/ B_data_to_read[3];
-
-	/* TO-DO: MAKE MODULES TO ACCESS B-RAMs */
-	
-
-	// --- instantiate 4 byte-wide BRAMs (synchronous) ---
-
-    bram0 bram0 (
-        .clock(clock),
-        .wren(B_write_enable[0]),
-        .address(B_address[0]),
-        .data(B_data_to_write[0]),
-        .q(B_data_to_read[0])
-    );
-
-    bram1 bram1 (
-        .clock(clock),
-        .wren(B_write_enable[1]),
-        .address(B_address[1]),
-        .data(B_data_to_write[1]),
-        .q(B_data_to_read[1])
-    );
-    
-	 bram2 bram2 (
-        .clock(clock),
-        .wren(B_write_enable[2]),
-        .address(B_address[2]),
-        .data(B_data_to_write[2]),
-        .q(B_data_to_read[2])
-    );
-    
-	 bram3 bram3 (
-        .clock(clock),
-        .wren(B_write_enable[3]),
-        .address(B_address[3]),
-        .data(B_data_to_write[3]),
-        .q(B_data_to_read[3])
-    );
-
-
-endmodule
-	
-module memory_for_vga (
-	/* Inputs are from the execute/memory pipeline register */
-	input [31:0] data_to_store,
-	input [4:0] memory_access_code, 
-	input [31:0] memory_address,
-	input clock,			
-	input [1:0] prev_r,	
-	/* Outputs are sent to memory/writeback pipeline register, for memory instructions */
-	output [31:0] writeback_register_data,	// Unused, don't connnect to anything
-	output [1:0] r,
-	output [255:0] memory_first_32_bytes // Array of first 32 bytes in memory
-);
-	
-	wire store; // 1: storing in memory, 0: loading from memory
-	assign store = memory_access_code[4];
-	wire [3:0] byte_enable; // determines which 4 bytes will be written or stored
-	assign byte_enable = memory_access_code[3:0];
-
-	// Each B-RAM uses a 16 bit address and loads/stores bytes
-	wire [15:0] B_address [3:0];	// 4 addresses for loading/storing up to 4 bytes
-	wire [7:0] B_data_to_write [3:0];
-	wire [7:0] B_data_to_read [3:0];
-	wire [3:0] B_write_enable;
-
-	// bram3 => addresses 4k + 0,
-	// bram2 => addresses 4k + 1,
-	// bram1 => addresses 4k + 2,
-	// bram0 => addresses 4k + 3, k is an integer
-	
-	// Given a memory address, we need to find the right addresses in the BRAM
-	// remainder from memory address divided by 4
-	assign r = memory_address[1:0];
-	assign B_address[3] = (r < 2'd1) ? (memory_address[17:2]) : (memory_address[17:2] + 1);
-	assign B_address[2] = (r < 2'd2) ? (memory_address[17:2]) : (memory_address[17:2] + 1); 
-	assign B_address[1] = (r < 2'd3) ? (memory_address[17:2]) : (memory_address[17:2] + 1);
-	assign B_address[0] = (memory_address[17:2]); // r < 4 always
-
-
-	// The tricky part: which BRAM corresponds to each byte of the data to write?
-	// The assignments rotate based on the remainder
-	assign B_data_to_write[3] =
-	(r == 2'd0) ? data_to_store[31:24] :
-	(r == 2'd1) ? data_to_store[ 7: 0] :
-	(r == 2'd2) ? data_to_store[15: 8] :
-	/*r == 2'd3*/ data_to_store[23:16];
-
-	assign B_data_to_write[2] =
-	(r == 2'd0) ? data_to_store[23:16] :
-	(r == 2'd1) ? data_to_store[31:24] :
-	(r == 2'd2) ? data_to_store[ 7: 0] :
-	/*r == 2'd3*/ data_to_store[15: 8];
-
-	assign B_data_to_write[1] =
-	(r == 2'd0) ? data_to_store[15: 8] :
-	(r == 2'd1) ? data_to_store[23:16] :
-	(r == 2'd2) ? data_to_store[31:24] :
-	/*r == 2'd3*/ data_to_store[ 7: 0];
-
-	assign B_data_to_write[0] =
-	(r == 2'd0) ? data_to_store[ 7: 0] :
-	(r == 2'd1) ? data_to_store[15: 8] :
-	(r == 2'd2) ? data_to_store[23:16] :
-	/*r == 2'd3*/ data_to_store[31:24];
-
-	// Similarly, the write enable will also rotate, and is only enabled if we are storing
-	assign B_write_enable[3] = store & (
-	(r == 2'd0) ? byte_enable[3] :
-	(r == 2'd1) ? byte_enable[0] :
-	(r == 2'd2) ? byte_enable[1] :
-	/*r == 2'd3*/ byte_enable[2]
-	);
-
-	assign B_write_enable[2] = store & (
-	(r == 2'd0) ? byte_enable[2] :
-	(r == 2'd1) ? byte_enable[3] :
-	(r == 2'd2) ? byte_enable[0] :
-	/*r == 2'd3*/ byte_enable[1]
-	);
-
-	assign B_write_enable[1] = store & (
-	(r == 2'd0) ? byte_enable[1] :
-	(r == 2'd1) ? byte_enable[2] :
-	(r == 2'd2) ? byte_enable[3] :
-	/*r == 2'd3*/ byte_enable[0]
-	);
-
-	assign B_write_enable[0] = store & (
-	(r == 2'd0) ? byte_enable[0] :
-	(r == 2'd1) ? byte_enable[1] :
-	(r == 2'd2) ? byte_enable[2] :
-	/*r == 2'd3*/ byte_enable[3]
-	);
-
-	// The order of consecutive bytes we want to read from the BRAM also rotates
-	assign writeback_register_data[31:24] =
-    	(prev_r == 2'd0) ? B_data_to_read[3] :
-	(prev_r == 2'd1) ? B_data_to_read[0] :
-	(prev_r == 2'd2) ? B_data_to_read[1] :
-	/*prev_r == 2'd3*/ B_data_to_read[2];
-	
-	assign writeback_register_data[23:16] =
-	(prev_r == 2'd0) ? B_data_to_read[2] :
-	(prev_r == 2'd1) ? B_data_to_read[3] :
-	(prev_r == 2'd2) ? B_data_to_read[0] :
-	/*prev_r == 2'd3*/ B_data_to_read[1];
-	
-	assign writeback_register_data[15:8] =
-	(prev_r == 2'd0) ? B_data_to_read[1] :
-	(prev_r == 2'd1) ? B_data_to_read[2] :
-	(prev_r == 2'd2) ? B_data_to_read[3] :
-	/*prev_r == 2'd3*/ B_data_to_read[0];
-	
-	assign writeback_register_data[7:0] =
-    	(prev_r == 2'd0) ? B_data_to_read[0] :
-    	(prev_r == 2'd1) ? B_data_to_read[1] :
-    	(prev_r == 2'd2) ? B_data_to_read[2] :
-	/*prev_r == 2'd3*/ B_data_to_read[3];
-
-	/* NEW FOR VGA */
-
-	// --- instantiate 4 byte-wide BRAMs (synchronous) ---
-	wire [63:0] mem_4k_3;
-	wire [63:0] mem_4k_2;
-	wire [63:0] mem_4k_1;
-	wire [63:0] mem_4k_0;
-	
-	assign memory_first_32_bytes = {
-		mem_4k_0[7:0],   mem_4k_1[7:0],   mem_4k_2[7:0],   mem_4k_3[7:0],
-		mem_4k_0[15:8],  mem_4k_1[15:8],  mem_4k_2[15:8],  mem_4k_3[15:8],
-		mem_4k_0[23:16], mem_4k_1[23:16], mem_4k_2[23:16], mem_4k_3[23:16],
-		mem_4k_0[31:24], mem_4k_1[31:24], mem_4k_2[31:24], mem_4k_3[31:24],
-		mem_4k_0[39:32], mem_4k_1[39:32], mem_4k_2[39:32], mem_4k_3[39:32],
-		mem_4k_0[47:40], mem_4k_1[47:40], mem_4k_2[47:40], mem_4k_3[47:40],
-		mem_4k_0[55:48], mem_4k_1[55:48], mem_4k_2[55:48], mem_4k_3[55:48],
-		mem_4k_0[63:56], mem_4k_1[63:56], mem_4k_2[63:56], mem_4k_3[63:56]
-	};
-	
-    bram_8_bytes bram0 (
-        .clk(~clock),
-        .we(B_write_enable[0]),
-        .addr(B_address[0]),
-        .din(B_data_to_write[0]),
-        .dout(B_data_to_read[0]),
-		.memory_array(mem_4k_3)
-    );
-
-    bram_8_bytes bram1 (
-        .clk(~clock),
-        .we(B_write_enable[1]),
-        .addr(B_address[1]),
-        .din(B_data_to_write[1]),
-        .dout(B_data_to_read[1]),
-		.memory_array(mem_4k_2)
-    );
-
-    bram_8_bytes bram2 (
-        .clk(~clock),
-        .we(B_write_enable[2]),
-        .addr(B_address[2]),
-        .din(B_data_to_write[2]),
-        .dout(B_data_to_read[2]),
-		.memory_array(mem_4k_1)
-    );
-
-    bram_8_bytes bram3 (
-        .clk(~clock),
-        .we(B_write_enable[3]),
-        .addr(B_address[3]),
-        .din(B_data_to_write[3]),
-        .dout(B_data_to_read[3]),
-		.memory_array(mem_4k_0)
-    );
-	
-	
-
-endmodule
-
-// simple_sync_bram.v
-// Generic single-port synchronous BRAM model for simulation
-
-module bram_8_bytes (
-    input  wire        clk,
-    input  wire        we,
-    input  wire [15:0] addr,
-    input  wire [7:0]  din,
-    output reg  [7:0]  dout,
-    output wire [63:0] memory_array  
-);
-
-    // memory array (8 bytes)
-    reg [7:0] mem [7:0];
-
-    always @(posedge clk) begin
-        if (we && (addr < 16'd8))
-            mem[addr] <= din;
-        dout <= mem[addr];
-    end
-
-    assign memory_array[7:0]    = mem[0];
-    assign memory_array[15:8]   = mem[1];
-    assign memory_array[23:16]  = mem[2];
-    assign memory_array[31:24]  = mem[3];
-    assign memory_array[39:32]  = mem[4];
-    assign memory_array[47:40]  = mem[5];
-    assign memory_array[55:48]  = mem[6];
-    assign memory_array[63:56]  = mem[7];
-
 endmodule
